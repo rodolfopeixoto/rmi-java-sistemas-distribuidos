b30fd5342a06b800f6f0fc75100a3d34d3e3c7c0
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-12-12 21:55:34 -0200

    Added client

--- HelloClient.java
+++ HelloClient.java
@@ -12,4 +12,17 @@ public class HelloClient {
 
 	// the Hello interface
 	static hello obj = null;
+
+	//The Hello interface
+	static Hello obj = null;
+
+	public static void main(String[] args) {
+      try{
+      	obj = (Hello)Naming.lookup("//"+"kvist.cs.umu.se"+"/Hello");
+      	System.out.println("Messagem no servidor RMI de: \"" + message + "\"");
+      }catch(Exception e){
+        System.out.println("HelloClient exception: " + e.getMessage());
+        e.printStackTrace();
+      }
+	}
 }
